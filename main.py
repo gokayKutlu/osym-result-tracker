a58818f73077ae20076ede2ef58ee294b3aabae9
Gökay Kutlu <gokaykutlu@hotmail.com>
2021-07-26 10:10:58 +0300

    Define a function to send a message on Telegram

--- main.py
+++ main.py
@@ -6,5 +6,11 @@ from bs4 import BeautifulSoup
 # variables and stuff
 URL = "https://sonuc.osym.gov.tr/"
 EXAMS_TO_CHECK = ["DGS", "YKS"]
-TG_API_TOKEN = os.getenv("TG_API_TOKEN")
-TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+
+
+def send_message(message):
+    TG_API_TOKEN = os.getenv("TG_API_TOKEN")
+    TG_CHAT_ID = os.getenv("TG_CHAT_ID")
+    requests.get(f"https://api.telegram.org/bot{TG_API_TOKEN}/sendMessage?chat_id={TG_CHAT_ID}&text={message}")
+
+send_message("test")
