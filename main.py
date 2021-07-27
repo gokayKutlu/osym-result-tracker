8e4563997a0a08ca773c8aa8fb208add12c70c8f
Gökay Kutlu <gokaykutlu@hotmail.com>
2021-07-27 10:08:17 +0300

    Add LIFX integration for notification and date time on every line

--- main.py
+++ main.py
@@ -8,11 +8,27 @@ from bs4 import BeautifulSoup
 URL = "https://sonuc.osym.gov.tr/"
 EXAMS_TO_CHECK = ["DGS", "YKS"]
 SLEEP_TIME = 3
+internet = True
 
 def send_message(message):
     TG_API_TOKEN = os.getenv("TG_API_TOKEN")
     TG_CHAT_ID = os.getenv("TG_CHAT_ID")
     requests.get(f"https://api.telegram.org/bot{TG_API_TOKEN}/sendMessage?chat_id={TG_CHAT_ID}&text={message}")
+
+
+def lifx_notification():
+    headers = {
+        "Authorization": f"Bearer {os.getenv('LIFX_TOKEN')}",
+    }
+
+    payload = {
+        "period": 2,
+        "cycles": 5,
+        "color": "red",
+    }
+
+    requests.post('https://api.lifx.com/v1/lights/all/effects/breathe', headers=headers, data=payload)
+
 
 while True:
     try:
@@ -23,13 +39,18 @@ while True:
         if len(EXAMS_TO_CHECK) == 0:
             send_message("Tum sinavlar aciklandi, sistem calismayi durduruyor.")
             break
+        
+        if internet == False:
+            print("internet gitmisti, simdi geldi.")
+            internet = True
 
         for exam in EXAMS_TO_CHECK:
             if exam in str(last_announced_exam).upper():
                 send_message(f"{exam} ACIKLANDI! {URL}")
                 EXAMS_TO_CHECK.remove(exam)
             else:
-                print(f"{exam} henuz aciklanmamis.")
+                date = datetime.strptime(datetime.now().strftime('%Y-%m-%d %H:%M:%S'), '%Y-%m-%d %H:%M:%S')
+                print(f"{exam} henuz aciklanmamis. {date}")
         
         time.sleep(SLEEP_TIME)
 
@@ -40,3 +61,4 @@ while True:
             send_message(f"{HATA}: {str(e)}")
         except:
             print("internet gitti")
+            internet = False
